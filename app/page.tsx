55f3c7fc4384db692f22e93688671ba1f7e0a463
Saibalaji Kokate <saikokatelearning@gmail.com>
2023-11-05 19:36:18 +0530

    add wikisearcg functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "./page.module.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main>Wikisearch App</main>;
+  return <main className={inter.className}></main>;
 }
